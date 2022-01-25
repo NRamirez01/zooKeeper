99f0889297a1acf8bdf2fdd5608801bb81ff0af7
Nathaniel Ramirez <nramire01@gmail.com>
2022-01-24 22:37:18 -0600

    update

--- src/zookeeper/Mammal.java
+++ src/zookeeper/Mammal.java
@@ -3,10 +3,6 @@ package zookeeper;
 public class Mammal {
 	protected static int energyLevel = 100;
 	
-	public void setEnergyLevel(int energyLevel) {
-		this.energyLevel = energyLevel;
-	}
-	
 	public int displayEnergy(){
 		System.out.println(energyLevel);
 		return energyLevel;
